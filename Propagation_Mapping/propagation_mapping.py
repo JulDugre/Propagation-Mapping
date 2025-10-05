b368ee68272574143cded2bde8a478d6f965f718
Jules R Dugré <67755081+JulDugre@users.noreply.github.com>
2025-10-05 14:09:19 -0400

    Update propagation_mapping.py

--- Propagation_Mapping/propagation_mapping.py
+++ Propagation_Mapping/propagation_mapping.py
@@ -382,9 +382,9 @@ if st.session_state.launch_btn:
             progress_text.text(f"Another sip  ☕, Processing subject {idx + 1} of {n_subjects}: {filename}")
 			
         # --- Save all pred accuracies ---
-        all_acc_df = pd.DataFrame(pred_accuracy, index=st.session_state.col_names, columns=["Spearman_r"]).to_csv(results_dir / "accuracy.csv")
+        all_acc_df = pd.DataFrame(pred_accuracy, index=st.session_state.col_names, columns=["Spearman_r"]).to_csv(results_dir / f"{atlas_choice}_accuracy.csv")
         all_acc_file = output_folder / "prediction_accuracies.csv"
-        st.session_state.saved_files.extend([results_dir / "accuracy.csv"])
+        st.session_state.saved_files.extend([results_dir / f"{atlas_choice}_accuracy.csv"])
 		
         # --- Show summary ---
         st.success("🚀 Propagation mapping complete!")
