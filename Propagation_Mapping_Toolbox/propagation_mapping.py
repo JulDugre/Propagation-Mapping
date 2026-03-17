26fe02177ac9fd5306383bcd52c7d2d51f03e295
Jules R Dugré <67755081+JulDugre@users.noreply.github.com>
2026-03-17 12:13:49 -0400

    Update propagation_mapping.py

--- Propagation_Mapping_Toolbox/propagation_mapping.py
+++ Propagation_Mapping_Toolbox/propagation_mapping.py
@@ -27,7 +27,7 @@ st.set_page_config(page_title="Propagation Mapping Toolbox", layout="wide")
 st.title("Propagation Mapping Toolbox (v0.3.1)")
 
 st.markdown("##### Please cite:")
-st.markdown("• Dugré, JR. (2025). Propagation Mapping: A Framework for Modeling Whole-Brain Propagation Patterns of Task-Evoked Activity. *bioRxiv*, DOI: <a href="https://pubmed.ncbi.nlm.nih.gov/27723746/" target="_blank">10.1101/2025.09.27.678975</a>)
+st.markdown("• Dugré, JR. (2025). Propagation Mapping: A Framework for Modeling Whole-Brain Propagation Patterns of Task-Evoked Activity. *bioRxiv*, DOI: [10.1101/2025.09.27.678975](https://www.biorxiv.org/content/10.1101/2025.09.27.678975v2)")
 st.markdown(
     """
     see also:<br>
